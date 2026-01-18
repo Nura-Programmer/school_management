7ca5e2a8297a689da899074226d4c397be262b3a
Nura Programmer <nuraprogrammer@gmail.com>
2026-01-18 16:42:07 +0100

    feat(prisma): add school model and initial migration

---
+++ packages/server/prisma/migrations/20260118153636_init_school/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "School" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);

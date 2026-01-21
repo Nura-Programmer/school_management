d5594a7626a17440db0da7d38b6f65aa806d4b48
Nura Programmer <nuraprogrammer@gmail.com>
2026-01-21 17:39:06 +0100

    fix(server): add url encoding middleware

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -10,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/schools', schoolRoutes);
 app.use('/teachers', teacherRoutes);

263e848fd1a360a41cde9b1f90ed8111627b4dbd
Nura Programmer <nuraprogrammer@gmail.com>
2026-01-23 00:40:40 +0100

    refactor(db): simplify prisma client for mysql

--- packages/server/src/prisma/client.ts
+++ packages/server/src/prisma/client.ts
@@ -1,20 +1,7 @@
 import 'dotenv/config';
-import { PrismaLibSql } from '@prisma/adapter-libsql';
 import { PrismaClient } from '@prisma/client';
 
-const { ENV, DATABASE_URL, TEST_DATABASE_URL } = process.env;
 
-const databaseUrl = ENV === 'test'
-    ? TEST_DATABASE_URL ?? ""
-    : DATABASE_URL ?? "";
-
-
-const adapter = new PrismaLibSql({
-    url: databaseUrl,
-});
-
-const prisma = new PrismaClient({
-    adapter,
-});
+const prisma = new PrismaClient();
 
 export default prisma;

b946bf0bc2ac902d6b07e1fa8c7147784346ce92
Nura Programmer <nuraprogrammer@gmail.com>
2026-02-01 05:35:07 +0100

    test(server): add transactional lifecycle hooks

--- packages/server/tests/setup.ts
+++ packages/server/tests/setup.ts
@@ -1,4 +1,14 @@
 import { execSync } from 'child_process';
+import { beforeEach, afterEach } from 'vitest';
+import { beginTestTransaction, rollbackTestTransaction } from './helpers/testPrisma';
+
+beforeEach(async () => {
+    await beginTestTransaction();
+});
+
+afterEach(async () => {
+    await rollbackTestTransaction();
+});
 
 export default async () => {
     execSync("bun prisma migrate deploy", {

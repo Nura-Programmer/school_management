77c0964d488f54c83a1fa3924f4b2c365f445ba7
Nura Programmer <nuraprogrammer@gmail.com>
2026-02-05 15:49:38 +0100

    refactor(db): construct database_url from db details in env file

--- packages/server/prisma.config.ts
+++ packages/server/prisma.config.ts
@@ -1,12 +1,23 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const {
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+    DB_USER,
+    DB_PASS,
+} = process.env;
+
+const DATABASE_URL =
+    `mysql://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
 export default defineConfig({
     schema: "src/prisma/schema.prisma",
     migrations: {
         path: "src/prisma/migrations",
     },
     datasource: {
-        url: process.env.DATABASE_URL,
-    },
+        url: DATABASE_URL,
+    }
 });

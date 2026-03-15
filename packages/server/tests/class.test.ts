d2267b5604bdffb697592fa71d27e6e7573686bb
Nura Programmer <nuraprogrammer@gmail.com>
2026-03-15 17:21:18 +0100

    test(server): Add failing test for creating a class under a school

---
+++ packages/server/tests/class.test.ts
@@ -0,0 +1,28 @@
+import request from "supertest";
+import { describe, it, expect } from "vitest";
+import { withTestPrisma } from "./helpers/withTestPrisma";
+import app from "../src/app";
+
+describe("Class API", () => {
+    it("Schould create a class under a school", async () => {
+        const { body } = await withTestPrisma(
+            request(app)
+                .post("/schools")
+                .send({
+                    name: "Annur International School",
+                    address: "123 Main St, Cityville",
+                })
+        );
+
+        const response = await withTestPrisma(request
+            (app).post(`/schools/${body.id}`)
+            .send({
+                name: "JSS 1"
+            })
+        );
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty("id");
+        expect(response.body.name).toBe("JSS 1");
+    });
+})

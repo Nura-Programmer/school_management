5d79c6c164c541354294f47146a79dc7bfd19cf4
Nura Programmer <nuraprogrammer@gmail.com>
2026-03-19 10:33:39 +0100

    feat(server): implement basic creating student

--- packages/server/src/controllers/student.controller.ts
+++ packages/server/src/controllers/student.controller.ts
@@ -1,3 +1,23 @@
-export const createStudent = () => {
-    return { body: { id: 1, name: "nura", classId: 1, schoolId: 1 } }
+import type { NextFunction, Request, Response } from "express"
+import { getPrisma } from "../prisma/getPrisma";
+
+export const createStudent = async (req: Request, res: Response, next: NextFunction) => {
+    const { params } = req;
+    const {name, classType} = req.body;
+    const schoolId = Number(params.schoolId);
+    const classId = Number(params.classId);
+
+    const prisma = getPrisma(req);
+
+    const newStudent = await prisma.student.create({
+        data: {
+            classId,
+            classType,
+            schoolId,
+            name
+        }
+    });
+
+    return res.status(201).json(newStudent);
+
 }

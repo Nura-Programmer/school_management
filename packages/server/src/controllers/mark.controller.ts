891a5955b8480146ead02381570a4e7678c12813
Nura Programmer <nuraprogrammer@gmail.com>
2026-03-19 22:05:53 +0100

    feat(server): implement mark creation for a student

--- packages/server/src/controllers/mark.controller.ts
+++ packages/server/src/controllers/mark.controller.ts
@@ -1,8 +1,26 @@
 import type { NextFunction, Request, Response } from "express";
+import { getPrisma } from "../prisma/getPrisma";
 
-export const createMark = (req: Request, res: Response, next: NextFunction) => {
-    res.status(201).json({
-        id: 1,
-        ...req.body
+export const createMark = async (req: Request, res: Response, next: NextFunction) => {
+    const {
+        ca,
+        test,
+        exam,
+        studentId,
+        subjectId
+    } = req.body;
+
+    const prisma = getPrisma(req);
+
+    const newMark = await prisma.mark.create({
+        data: {
+            ca,
+            test,
+            exam,
+            studentId,
+            subjectId
+        }
     });
+
+    res.status(201).json(newMark);
 }

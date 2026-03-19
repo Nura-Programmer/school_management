c0387150a1cab0245c05b5601535097e409ea526
Nura Programmer <nuraprogrammer@gmail.com>
2026-03-19 10:46:30 +0100

    feat(server): implement data validation before creating student

--- packages/server/src/controllers/student.controller.ts
+++ packages/server/src/controllers/student.controller.ts
@@ -1,13 +1,20 @@
 import type { NextFunction, Request, Response } from "express"
 import { getPrisma } from "../prisma/getPrisma";
+import { createStudentSchema } from "../schemas/student.schema";
 
 export const createStudent = async (req: Request, res: Response, next: NextFunction) => {
-    const { params } = req;
-    const {name, classType} = req.body;
-    const schoolId = Number(params.schoolId);
-    const classId = Number(params.classId);
+    const { params, body } = req;
+    const validatePayload = createStudentSchema.safeParse({ ...req.params, ...req.body });
+
+    if (!validatePayload.success) {
+        return res.status(400).json({
+            error: "ValidationError",
+            details: validatePayload.error?.flatten
+        });
+    }
 
     const prisma = getPrisma(req);
+    const { name, classType, classId, schoolId } = validatePayload.data;
 
     const newStudent = await prisma.student.create({
         data: {
---
+++ packages/server/src/schemas/student.schema.ts
@@ -0,0 +1,8 @@
+import z from "zod";
+
+export const createStudentSchema = z.object({
+    name: z.string().min(3, "Name must not be less than 3 characters").max(100, "Name must not exceed 100 characters."),
+    classType: z.string().length(1, "Class type must be a single character. e.g A, B, C."),
+    classId: z.number(),
+    schoolId: z.number()
+})

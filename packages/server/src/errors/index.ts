b0db685993f7b1a62db3e06f11e318d63e4a423a
Nura Programmer <nuraprogrammer@gmail.com>
2026-03-20 15:22:26 +0100

    refactor(server): add re-usable errors class

---
+++ packages/server/src/errors/index.ts
@@ -0,0 +1,39 @@
+import type { Response } from "express";
+
+type ResponseType = {
+    error: string,
+    message: string
+}
+
+class Errors {
+    name: string;
+    res: Response;
+
+    constructor(res: Response, name: string) {
+        this.name = name;
+        this.res = res;
+    }
+
+    conflict = (): Response => {
+        return this.res.status(409).json({
+            error: "Conflict",
+            message: `${this.name} already exist.`
+        });
+    }
+
+    server = (): Response => {
+        return this.res.status(500).json({
+            error: "ServerError",
+            message: "An internal server error occur. Please try again."
+        });
+    }
+
+    validation = (message: string): Response => {
+        return this.res.status(400).json({
+            error: "ValidationError",
+            message
+        });
+    }
+}
+
+export default Errors;

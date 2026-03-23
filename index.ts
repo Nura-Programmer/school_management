1fb07cddf03d1f5ef9da1e73c852e460cded2a4c
Nura Programmer <nuraprogrammer@gmail.com>
2026-03-23 07:12:52 +0100

    feat(client|server): run both frontend and the backend

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import concurrently from 'concurrently';
+
+concurrently([
+    { 
+        name: 'server', 
+        cwd: 'packages/server',
+        command: 'npm run dev', 
+        prefixColor: 'blue' 
+    },
+    { 
+        name: 'client', 
+        cwd: 'packages/client',
+        command: 'npm run dev', 
+        prefixColor: 'green' 
+    }
+]);

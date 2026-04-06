a9b7609fc053f2c38e355456a30f493ce7e04ee4
Nura Programmer <nuraprogrammer@gmail.com>
2026-04-06 09:30:59 +0100

    chore(server): use wrapper in mark controller

--- packages/server/src/controllers/mark.controller.ts
+++ packages/server/src/controllers/mark.controller.ts
@@ -1,69 +1,54 @@
-import type { Request, Response } from 'express';
-import { getPrisma } from '../prisma/getPrisma';
 import { CreateMarkSchema, UpdateMarkSchema } from '../schemas/mark.schema';
-import Errors from '../errors';
+import Wrapper from '../middleware/wrapper';
 
-export const createMark = async (req: Request, res: Response) => {
-   const errors = new Errors(res, 'Mark');
+const { withTryCatch } = new Wrapper("Marks");
 
-   try {
-      const validatePayload = CreateMarkSchema.safeParse(req.body);
-      if (!validatePayload.success) {
-         return errors.validation(validatePayload.error.message);
-      }
+export const createMark = withTryCatch(async (handlers, prisma, errors) => {
+   const { req, res, next } = handlers;
 
-      const prisma = getPrisma(req);
-      const { ca, test, exam, studentId, subjectId } = validatePayload.data;
-
-      const markExist = await prisma.mark.findFirst({
-         where: { studentId, subjectId },
-      });
-      if (markExist) return errors.conflict();
-
-      const newMark = await prisma.mark.create({
-         data: {
-            ca: Number(ca),
-            test: Number(test),
-            exam: Number(exam),
-            studentId,
-            subjectId,
-         },
-      });
-
-      res.status(201).json(newMark);
-   } catch (error) {
-      console.error(error);
-
-      return errors.server();
+   const validatePayload = CreateMarkSchema.safeParse(req.body);
+   if (!validatePayload.success) {
+      return errors.validation(validatePayload.error.message);
    }
-};
 
-export const updateMark = async (req: Request, res: Response) => {
-   const errors = new Errors(res, 'Mark');
+   const { ca, test, exam, studentId, subjectId } = validatePayload.data;
 
-   try {
-      const validatePayload = UpdateMarkSchema.safeParse(req.body);
-      if (!validatePayload.success) {
-         return errors.validation(validatePayload.error.message);
-      }
+   const markExist = await prisma.mark.findFirst({
+      where: { studentId, subjectId },
+   });
+   if (markExist) return errors.conflict();
 
-      const prisma = getPrisma(req);
-      const { ca, test, exam, id } = validatePayload.data;
+   const newMark = await prisma.mark.create({
+      data: {
+         ca: Number(ca),
+         test: Number(test),
+         exam: Number(exam),
+         studentId,
+         subjectId,
+      },
+   });
 
-      const markExist = await prisma.mark.findFirst({
-         where: { id },
-      });
-      if (!markExist) return errors.notFound();
+   res.status(201).json(newMark);
+});
 
-      const updatedMark = await prisma.mark.update({
-         where: { id },
-         data: { ca, test, exam },
-      });
+export const updateMark = withTryCatch(async (handlers, prisma, errors) => {
+   const { req, res, next } = handlers;
 
-      res.status(200).json(updatedMark);
-   } catch (error) {
-      console.error(error);
-
-      return errors.server();
+   const validatePayload = UpdateMarkSchema.safeParse(req.body);
+   if (!validatePayload.success) {
+      return errors.validation(validatePayload.error.message);
    }
-};
+
+   const { ca, test, exam, id } = validatePayload.data;
+   const markExist = await prisma.mark.findFirst({
+      where: { id },
+   });
+   if (!markExist) return errors.notFound();
+
+   const updatedMark = await prisma.mark.update({
+      where: { id },
+      data: { ca, test, exam },
+   });
+
+   res.status(200).json(updatedMark);
+});

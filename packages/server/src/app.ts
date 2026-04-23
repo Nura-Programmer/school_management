d67404596cc84a9f57600e89b08ae9a077964b1a
Nura Programmer <nuraprogrammer@gmail.com>
2026-04-23 15:43:45 +0100

    chore(server): remove x-powered-by from the response header

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -6,6 +6,7 @@ import { errorHandler } from './middleware/error.middleware';
 import testRoutes from '../tests/__test__.routes';
 
 const app = express();
+app.disable('x-powered-by');
 
 const PORT = process.env.PORT || 3000;
 
